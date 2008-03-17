ef4e1abc3dc35d2c761e494fa8f8efcbc72df855
Dirk Breuer <dbreuer@dbreuer.local>
2008-03-18 00:54:36 +0100

    CHANGED - some more mocha examples

--- advanced/source/testing/mocha_examples.rb
+++ advanced/source/testing/mocha_examples.rb
@@ -1,14 +1,17 @@
+require 'rubygems'
 require 'test/unit'
 require 'mocha'
 
 class ArticlesControllerTest < Test::Unit::TestCase
 
+  # Klassenmethoden mocken
   def test_should_list_all_articles_on_index_action
     Article.expects(:find).with(:all).returns([])
     get :index
     assert_response :success, @response.body
   end
   
+  # Instanzmethoden mocken
   def test_should_display_title_and_content_on_show_action
     article = Article.new
     article.expects(:title).returns("Mocking with mocha")
@@ -17,6 +20,60 @@ class ArticlesControllerTest < Test::Unit::TestCase
     get :show, :id => 1
     assert_response :success, @response.body
     assert_template 'show'
+  end
+  
+end
+
+# Aufwendigeres Beispiel mit Stubben auf allen Instanzen
+class RemoteSystem
+  
+  def query_for_a_value_of_an_id(value, id)
+    connection = establish_connection(configuration)
+    result = connection.execute(Query.new(string))
+    result.sort.first
+  end
+  
+end
+
+class Account
+  
+  attr_reader :system
+  
+  def initialize(any_system)
+    @system = any_system
+  end
+  
+  def balance
+    system.query_for_a_value_of_an_id('balance', self.id)
+  end
+  
+end
+
+class AccountTest < Test::Unit::TestCase
+  
+  def test_should_get_balance_of_account_from_remote_system
+    RemoteSystem.any_instance.stubs(:query_for_a_value_of_an_id).returns(100)
+    account = Account.new(RemoteSystem.new)
+    assert_equal 100, account.balance
+  end
+  
+end
+
+# 'Klassisches' Mocken
+class Order
+  def self.fetch_most_recent_order(database)
+    database.query("SELECT * FROM orders ORDER BY created_at LIMIT 1")
+  end
+  
+  # ... more Methods
+end
+
+class OrderTest < Test::Unit::TestCase
+  
+  def test_should_fetch_most_recent_order_from_database
+    mock_database = mock('mock_database', :query => 'order')
+    order = Order.fetch_most_recent_order(mock_database)
+    assert_equal "order", order
   end
   
 end
